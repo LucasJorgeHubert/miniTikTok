ca91cf5b33c390ac582df2561f38c938f926618d
Lucas Jorge Hubert <lucasjhubert@gmail.com>
2024-04-16 11:32:03 -0300

    Initial Commit

---
+++ miniTikTok/miniTikTokApp.swift
@@ -0,0 +1,17 @@
+//
+//  miniTikTokApp.swift
+//  miniTikTok
+//
+//  Created by Lucas Hubert on 16/04/24.
+//
+
+import SwiftUI
+
+@main
+struct miniTikTokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
